379fff5c5230a81b54391e8940ecafe3271692f5
p.capasso72 <p.capasso72@gmail.com>
2022-05-13 14:41:43 +0200

    Added javadoc for SCIManager.java and Pattern.java

--- src/main/java/it/unisa/di/dif/SCIManager.java
+++ src/main/java/it/unisa/di/dif/SCIManager.java
@@ -9,7 +9,22 @@ import it.unisa.di.dif.utils.AdaptationMethod;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
+/**
+ * It's a class that contains static methods for extracting the reference pattern from a list of images, extracting the
+ * residual noise from an image, and comparing the reference pattern with the residual noise or reference pattern
+ */
 public abstract class SCIManager {
+
+    /**
+     * Extracting the reference pattern from a list of images using a filter for noise extraction.
+     * It takes a list of images, a filter, and an adaptation method, and returns a reference pattern
+     *
+     * @param images ArrayList of images to be used to create the reference pattern
+     * @param filter the filter to be used for the extraction of the residual noise.
+     * @param method The method used to adapt the images to the same size.
+     * @return A reference pattern.
+     */
     public static ReferencePattern extractReferencePattern(ArrayList<Image> images, Filter filter, AdaptationMethod method) {
         if(method == AdaptationMethod.RESIZE) {
             throw new UnsupportedOperationException("Resize method is not supported yet");
@@ -61,10 +76,36 @@ public abstract class SCIManager {
         return rp;
     }
 
+
+    /**
+     * Extracts a reference pattern from a list of images using a filter and an adaptation method.
+     *
+     * @param images An ArrayList of Image objects.
+     * @param filter The filter to use.
+     * @return A reference pattern.
+     */
     public static ReferencePattern extractReferencePattern(ArrayList<Image> images, Filter filter) {
         return extractReferencePattern(images, filter, AdaptationMethod.NOT_ADAPT);
     }
 
+    /**
+     * Extracts a reference pattern from a list of images using the default filter and the specified adaptation method
+     *
+     * @param images An ArrayList of Image objects.
+     * @param method The method used to adapt the size of the images.
+     * @return A reference pattern.
+     */
+    public static ReferencePattern extractReferencePattern(ArrayList<Image> images, AdaptationMethod method) {
+        return extractReferencePattern(images, FilterFactory.getDefaultFilter(), method);
+    }
+
+    /**
+     * Extracting the residual noise from an image using a filter.
+     *
+     * @param image the image from which to extract residual noise
+     * @param filter The filter to use.
+     * @return ResidualNoise
+     */
     public static ResidualNoise extractResidualNoise(Image image, Filter filter) {
         Image filteredImage = new Image();
         for (ColorChannel.Channel channel : ColorChannel.Channel.values()) {
@@ -84,22 +125,55 @@ public abstract class SCIManager {
         return ResidualNoise.getInstanceBySubtraction(image, filteredImage);
     }
 
+    /**
+     * This function extracts the reference pattern from a list of images using the default filter and the default adaptation method.
+     * @param images The images to extract the reference pattern from.
+     * @return A reference pattern.
+     */
     public static ReferencePattern extractReferencePattern(ArrayList<Image> images) {
         return extractReferencePattern(images, FilterFactory.getDefaultFilter(), AdaptationMethod.NOT_ADAPT);
     }
 
+
+    /**
+     * It takes an image and a filter and returns a ResidualNoise object.
+     * @param image The image to extract the residual noise from.
+     * @return A ResidualNoise object.
+     */
     public static ResidualNoise extractResidualNoise(Image image) {
         return extractResidualNoise(image, FilterFactory.getDefaultFilter());
     }
 
+    /**
+     * It takes an image and returns a ResidualNoise object
+     *
+     * @param path The path to the image file.
+     * @return A ResidualNoise object.
+     */
     public static ResidualNoise extractResidualNoise(String path) throws IOException {
-        return extractResidualNoise(new Image(path), FilterFactory.getDefaultFilter());
-    }
-
-    public static ResidualNoise extractResidualNoise(String path, Filter filter) throws IOException {
         return extractResidualNoise(new Image(path));
     }
 
+    /**
+     * It takes an image and a filter, and returns the residual noise of the image after applying the filter
+     *
+     * @param path The path to the image file.
+     * @param filter The filter to use.
+     * @return ResidualNoise
+     */
+    public static ResidualNoise extractResidualNoise(String path, Filter filter) throws IOException {
+        return extractResidualNoise(new Image(path), filter);
+    }
+
+    /**
+     * It takes two noise patterns and a method of adaptation, and returns the Pearson correlation coefficient between the
+     * two patterns
+     *
+     * @param referencePattern The reference pattern that you want to compare against.
+     * @param residualNoise The noise pattern that you want to compare to the reference pattern.
+     * @param method The method to use when the reference pattern and residual noise have different sizes.
+     * @return The Pearson correlation coefficient.
+     */
     public static double compare(ReferencePattern referencePattern, NoisePattern residualNoise, AdaptationMethod method) {
         if(!referencePattern.equalsSize(residualNoise)) {
             if(method == AdaptationMethod.NOT_ADAPT) {
@@ -118,6 +192,13 @@ public abstract class SCIManager {
         return p.getCorrelationCoefficient();
     }
 
+    /**
+     * This function compares a reference pattern to a noise pattern and returns a similarity score
+     *
+     * @param referencePattern The reference pattern that you want to compare against.
+     * @param residualNoise The noise pattern to be compared to the reference pattern.
+     * @return The similarity between the reference pattern and the residual noise.
+     */
     public static double compare(ReferencePattern referencePattern, NoisePattern residualNoise) {
         return compare(referencePattern, residualNoise, AdaptationMethod.NOT_ADAPT);
     }
